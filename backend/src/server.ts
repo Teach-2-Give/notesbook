702559b5503b3ff30ba0b497859d4616517482dd
victorpreston <prestonvictor25@gmail.com>
2024-06-07 21:26:12 +0300

    feat: add health check endpoint

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,7 @@ import pool from './db';
 import notesRoutes from './routes/notesRoutes';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
+import healthRoutes from './routes/healthRoutes';
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ const swaggerDocument = YAML.load('./src/swagger.yaml');
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(notesRoutes);
+app.use(healthRoutes);
 
 app.get('/', async (req, res) => {
   try {
